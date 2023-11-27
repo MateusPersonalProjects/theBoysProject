b761c471eab4a456d6765d37b7668b8c812ec467
Mateus Oliveira <mateusaugusto.godoy@gmail.com>
2023-11-26 22:53:46 -0300

    comentei entidades

--- entidades.c
+++ entidades.c
@@ -4,22 +4,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+ * Inicializa um heroi
+ * Retorna true se foi possivel alocar memória, se não retorna false
+ */
 bool startHeroi(Heroi **heroi, int id) {
   Set *habilidades;
   int quantHab, hab;
 
+  // Aloca memoria para o heroi
   *heroi = (Heroi *)malloc(sizeof(Heroi));
   if (*heroi == NULL) return false;
 
+  // Define seus atributos padroes
   (*heroi)->id = id;
   (*heroi)->exp = 0;
   (*heroi)->paciencia = randomInteger(0, 100);
   (*heroi)->velocidade = randomInteger(50, 5000);
 
+  // Define Sua quantidade de habilidades
   quantHab = randomInteger(1, 3);
 
-  startSet(&habilidades);
+  // Inicializa o conjunto de habilidades do heroi
+  if (!startSet(&habilidades)) return false;
 
+  // Insere habilidades do conjunto de habilidades
   for (int i = 0; i < quantHab; i++) {
     hab = randomInteger(0, N_HABILIDADES);
     insertEndLL(&(habilidades->elementos), hab);
@@ -29,40 +38,56 @@ bool startHeroi(Heroi **heroi, int id) {
   return true;
 }
 
+/*
+ * Inicializa uma base
+ * Retorna true se foi possivel alocar memória, se não retorna false
+ */
 bool startBase(Base **base, int id) {
   Set *inside;
   Queue *line;
 
+  // Aloca memoria para a base
   *base = (Base *)malloc(sizeof(Base));
   if (*base == NULL) return false;
 
+  // Define atributos padroes
   (*base)->id = id;
   (*base)->local[0] = randomInteger(0, N_TAMANHO_MUNDO - 1);
   (*base)->local[1] = randomInteger(0, N_TAMANHO_MUNDO - 1);
 
-  startSet(&inside);
-  startQueue(&line, N_HEROIS);
+  // Inicializa o conjunto de herois dentro da base
+  if (!startSet(&inside)) return false;
+  // Inicializa a fila de espera
+  if (!startQueue(&line, N_HEROIS)) return false;
 
   (*base)->presentes = inside;
   (*base)->fila = line;
   return true;
 }
 
+/*
+ * Inicializa uma missao
+ * Retorna true se foi possivel alocar memória, se não retorna false
+ */
 bool startMissao(Missao **missao, int id) {
   Set *habilidades;
   int quantHab, hab;
 
+  // Aloca memoria para a missao
   *missao = (Missao *)malloc(sizeof(Missao));
   if (*missao == NULL) return false;
 
+  // Define atributos padroes
   (*missao)->id = id;
   (*missao)->local[0] = randomInteger(0, N_TAMANHO_MUNDO - 1);
   (*missao)->local[1] = randomInteger(0, N_TAMANHO_MUNDO - 1);
 
+  // Aloca memoria para o conjunto de habilidades
   if (!startSet(&habilidades)) return false;
 
   quantHab = randomInteger(6, 10);
 
+  // Define as habilidades
   for (int i = 0; i < quantHab; i++) {
     hab = randomInteger(0, N_HABILIDADES);
     insertEndLL(&(habilidades->elementos), hab);
@@ -72,21 +97,30 @@ bool startMissao(Missao **missao, int id) {
   return true;
 }
 
+/*
+ * Inicializa o mundo
+ * Retorna true se foi possivel alocar memória, se não retorna false
+ */
 bool startMundo(Mundo **mundo) {
+  // Aloca memoria para o mundo
   *mundo = (Mundo *)malloc(sizeof(Mundo));
   if (*mundo == NULL) return false;
 
+  // Define atributos padroes
   (*mundo)->nHerois = N_HEROIS;
   (*mundo)->nBases = N_BASES;
   (*mundo)->nMissoes = N_MISSOES;
   (*mundo)->nHabilidades = N_HABILIDADES;
 
+  // Inicializa herois do mundo
   for (int i = 0; i < (*mundo)->nHerois; i++)
     if (!startHeroi(&((*mundo)->Herois[i]), i)) return false;
 
+  // Inicializa bases do mundo
   for (int i = 0; i < (*mundo)->nBases; i++)
     if (!startBase(&((*mundo)->bases[i]), i)) return false;
 
+  // Inicializa Missoes do mundo
   for (int i = 0; i < (*mundo)->nMissoes; i++)
     if (!startMissao(&((*mundo)->missoes[i]), i)) return false;
 
